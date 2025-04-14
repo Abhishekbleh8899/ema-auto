623151970b06b16487f4fe20c01685f8c0fb7c72
Abhishekbleh8899 <97578706+Abhishekbleh8899@users.noreply.github.com>
2025-04-14 15:56:16 +0530

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -3,6 +3,6 @@ USE employees_db;
 CREATE TABLE IF NOT EXISTS EMPLOYEES (
     id INT AUTO_INCREMENT PRIMARY KEY,
     emp_name VARCHAR(255) NOT NULL,
-    contact VARCHAR(255) NOT NULL,
-    address TEXT NOT NULL
+    emp_contact VARCHAR(255) NOT NULL,
+    emp_add TEXT NOT NULL
 );
